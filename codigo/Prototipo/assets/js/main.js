535616a92556686130540aaada590d14c10a466a
Matheus-Spirit <matheusdiasmendespop@gmail.com>
2024-05-04 17:11:10 -0300

    Adição tela cadastro medico

--- codigo/Prototipo/assets/js/main.js
+++ codigo/Prototipo/assets/js/main.js
@@ -1,6 +1,8 @@
-const myCarouselElement = document.querySelector('#myCarousel')
+//funcionalidade do carrosel
+const myCarousel = document.querySelector('#carouselExampleDark')
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
+const carousel = new bootstrap.Carousel(myCarousel, {
   interval: 2000,
   touch: false
 })
+
